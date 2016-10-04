34268de3e6fe26a87ce4722d6d04e1d3aebcad6f
Tom <tsiener@gmx.de>
2016-10-04 23:06:34 +0200

    site.mk Version  ffwy-v016c

--- site.mk
+++ site.mk
@@ -121,7 +121,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwy-v016b
+DEFAULT_GLUON_RELEASE := ffwy-v016c
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
