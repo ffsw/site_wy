1022ac330690b97617425a448eea739e1aace342
Tom <tsiener@gmx.de>
2016-11-20 14:37:05 +0100

    site.mk -v019 mit Kanal 6

--- site.mk
+++ site.mk
@@ -148,7 +148,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwy-v018
+DEFAULT_GLUON_RELEASE := ffwy-v019
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
