be1ed30188acaf2b8e484095c5f5ad4eb2f20f32
Tom <tsiener@gmx.de>
2023-10-19 09:21:25 +0200

    Version v051f

--- site.mk
+++ site.mk
@@ -184,7 +184,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwy-v051
+DEFAULT_GLUON_RELEASE := ffwy-v051f
 
 
 #       GLUON_RELEASE
